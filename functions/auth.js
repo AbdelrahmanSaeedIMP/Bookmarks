038e3ab602ae7f387c0798ba6ee9f4e0f8c6bda2
AbdelrahmanSaeed11 <abdelrahmansaeed2101@gmail.com>
2022-12-06 22:13:02 +0200

    edit jwt signature

--- functions/auth.js
+++ functions/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const Auth = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
-        jwt.verify(token, process.env.jwtToken);
+        jwt.verify(token, process.env.jwtSecret);
         next();
     }
     catch (err) {
