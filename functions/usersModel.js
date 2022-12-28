dccb11f396e51b0cc7aa2d5343acfca6571283b4
AbdelrahmanSaeed11 <abdelrahmansaeed2101@gmail.com>
2022-12-28 18:57:01 +0200

    update sql command

--- functions/usersModel.js
+++ functions/usersModel.js
@@ -1,9 +1,9 @@
-const {pool} = require('./database.js');
+const { pool } = require('./database.js');
 
 class Users {
     async get_bookmarks(user_id) {
         try {
-            const [rows, fields] = await pool.execute('SELECT books.id, books.name FROM books INNER JOIN bookmarks ON bookmarks.user_id=? AND bookmarks.book_id=books.id;', [user_id]);
+            const [rows, fields] = await pool.execute('SELECT books.id, books.name, books.description, books.imgUrl FROM books INNER JOIN bookmarks ON bookmarks.user_id=? AND bookmarks.book_id=books.id;', [user_id]);
             return rows;
         }
         catch (err) {
