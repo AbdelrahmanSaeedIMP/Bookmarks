77f8066ba1e95f6e2f08a924dea3df7a436ed3df
AbdelrahmanSaeed11 <abdelrahmansaeed2101@gmail.com>
2022-12-28 18:57:52 +0200

    console message on get request

--- functions/users.js
+++ functions/users.js
@@ -8,6 +8,7 @@ const user = new Users();
 const userRouter = (app => {
     app.get('/user/bookmarks', Auth, async (req, res) => {
         try {
+            console.log('request sent from client');
             const userData = jwt.decode(req.headers.authorization.split(' ')[1]);
             const joiSchema = joi.object({
                 id: joi.number().required(),
