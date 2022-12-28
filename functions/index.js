65814950c72e726298b1c7ad5f6c001f1d7ca998
AbdelrahmanSaeed11 <abdelrahmansaeed2101@gmail.com>
2022-12-28 18:56:16 +0200

    update cors

--- functions/index.js
+++ functions/index.js
@@ -2,9 +2,11 @@ const functions = require("firebase-functions");
 const express = require('express');
 const bodyParser = require('body-parser');
 const { userRouter } = require('./users.js');
+const cors = require('cors')
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', (_req, res) => {
     res.send('Task');
