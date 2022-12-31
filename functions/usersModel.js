b30cd7295dd37ef27ca7e9f507500a0baaf75f56
AbdelrahmanSaeed11 <abdelrahmansaeed2101@gmail.com>
2022-12-31 18:11:53 +0200

    enhance sql query

--- functions/usersModel.js
+++ functions/usersModel.js
@@ -3,7 +3,7 @@ const { pool } = require('./database.js');
 class Users {
     async get_bookmarks(user_id) {
         try {
-            const [rows, fields] = await pool.execute('SELECT books.id, books.name, books.description, books.imgUrl FROM books INNER JOIN bookmarks ON bookmarks.user_id=? AND bookmarks.book_id=books.id;', [user_id]);
+            const [rows, fields] = await pool.execute('SELECT books.id, books.name, books.description, books.imgUrl, (case when bookmarks.book_id=books.id then true else false end) as bookmarked FROM books LEFT JOIN bookmarks ON bookmarks.user_id=? AND bookmarks.book_id=books.id;', [user_id]);
             return rows;
         }
         catch (err) {
